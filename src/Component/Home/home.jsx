a6771c83540f7037c028c64e7d3c23fc7ec41f1c
Leandro Martini <87733423+manrique1986@users.noreply.github.com>
2023-05-01 20:11:22 -0300

    estilos

--- src/Component/Home/home.jsx
+++ src/Component/Home/home.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const home = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 ">
+    <div className="grid grid-cols-1 sm:grid-cols-1   md:grid-cols-1 lg:grid-cols-2 ">
       <div className="">
-        <p className="text-center pt-44 p-56 text-3xl italic hyphens-manual">Traumatologo especialista en cadera</p>
+        <p className="text-center pt-44 p-20 text-3xl italic hyphens-manual">Traumatologo especialista en cadera</p>
       </div>
 
-      <div className=" sm:float-left ">
-        <img className="float-right pt-14 p-8 w-8/12 rounded-full"
+      <div className="">
+        <img className=" p-4 w-8/12 rounded-full  "
           src="https://res.cloudinary.com/dytpump6i/image/upload/v1680568914/roberto-valentini_ldp5bs.jpg"
           alt="texto"
         />
