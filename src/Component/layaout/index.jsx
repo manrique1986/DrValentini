7ca0e11f34ef142578c00a1ab994a4c054498990
Leandro Martini <87733423+manrique1986@users.noreply.github.com>
2023-06-11 21:38:58 -0300

    viendo

--- src/Component/layaout/index.jsx
+++ src/Component/layaout/index.jsx
@@ -1,34 +1,34 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 
-import { useLocation } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
 import NavBar from "../Dashboard";
 import Navbar from "../NavBar";
 const Layout = ({ children }) => {
   const [open, setOpen] = useState(false);
-  const [title, setTitle] = useState("");
+  const [title, ] = useState("");
 
-  const location = useLocation();
+  // const location = useLocation();
 
-  useEffect(() => {
-    switch (location.pathname) {
-        case "a":
-            setTitle()
+//   useEffect(() => {
+//     switch (location.pathname) {
+//         case "a":
+//             setTitle()
        
-        break;
-      case "/home":
-        setTitle("Dr Valentini");
-        break;
-      case "/SobreMi":
-        setTitle("Sobre mi");
-        break;
-      case "/PatologiasYcirugias":
-        setTitle("Patologias y cirugias");
-        break;
-      case "/contacto":
-        setTitle("Contacto");
-        break;
-    }
-  }, [location.pathname]);
+//         break;
+//       case "/home":
+//         setTitle("Dr Valentini");
+//         break;
+//       case "/SobreMi":
+//         setTitle("Sobre mi");
+//         break;
+//       case "/PatologiasYcirugias":
+//         setTitle("Patologias y cirugias");
+//         break;
+//       case "/contacto":
+//         setTitle("Contacto");
+//         break;
+//     }
+//   }, [location.pathname]);
 
   return (
     <div className="relative w-full z-10">
